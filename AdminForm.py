d1c33f61d433139e99a4f58fbe00fa022a6cd22b
Илья Левинский <ilyalevinskii@gmail.com>
2021-11-22 17:56:53 +0300

    added some functions to oper

--- AdminForm.py
+++ AdminForm.py
@@ -1,16 +1,94 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
+import mysql.connector as mc
+from PyQt5.QtWidgets import QMessageBox
+from PyQt5.QtWidgets import QTableWidgetItem
 
 import OperForm
 
 
 class Ui_AdminFom(object):
+
     def openOperForm(self):
         self.window = QtWidgets.QMainWindow()
-
         self.ui = OperForm.Ui_OperatorForm()
         self.ui.setupUi(self.window)
         self.window.show()
 
+    def showTableForUsersClick(self):
+        choosenTable = self.choosenTableForUsers.currentText()
+        query = "SELECT * FROM "
+        if choosenTable == "Пользователи":
+            query += "users;"
+            self.tableWidgetUsers.setColumnCount(4)
+            self.tableWidgetUsers.setHorizontalHeaderLabels(["Номер", "Логин", "Пароль", "Роль"])
+        elif choosenTable == "Роли":
+            query += "userroles;"
+            self.tableWidgetUsers.setColumnCount(2)
+            self.tableWidgetUsers.setHorizontalHeaderLabels(["Номер", "Роль"])
+        try:
+            usersDB = mc.connect(
+                host="localhost",
+                user="root",
+                password="root",
+                database="mydb"
+            )
+            result = ""
+            mycursor = usersDB.cursor()
+            mycursor.execute(query)
+            result = mycursor.fetchall()
+            self.tableWidgetUsers.setRowCount(0)
+
+            for row_number, row_data in enumerate(result):
+                self.tableWidgetUsers.insertRow(row_number)
+                for column_number, data in enumerate(row_data):
+                    self.tableWidgetUsers.setItem(row_number, column_number, QTableWidgetItem(str(data)))
+
+        except Exception as err:  # mc.Error
+            msg = QMessageBox()
+            msg.setIcon(QMessageBox.Critical)
+            msg.setWindowTitle("Ошибка")
+            msg.setText("Внимание")
+            msg.setInformativeText("Проверьте введенные данные!")
+            # msg.setInformativeText("Error: {0}".format(err))
+            msg.exec_()
+
+
+
+    def showTableForFactoryClick(self):
+        choosenTable = self.choosenTableForFactory.currentText()
+
+    def insertDataIntoUsers(self):
+        try:
+            query = "INSERT INTO users (Login, Password, Roles_idRoles) values (%s, %s, %s)"
+            login = self.LoginCreate.text()
+            password = self.PasswordCreate.text()
+            if self.UserRoleCreate.currentText() == "Оператор":
+                role = 2
+            elif self.UserRoleCreate.currentText() == "Администратор":
+                role = 1
+            elif self.UserRoleCreate.currentText() == "Разработчик модели":
+                role = 3
+            value = (login, password, role)
+            usersDB = mc.connect(
+                host="localhost",
+                user="root",
+                password="root",
+                database="mydb"
+            )
+            mycursor = usersDB.cursor()
+            mycursor.execute(query, value)
+            usersDB.commit()                # Обязательно для записи
+            msg = QMessageBox()
+            msg.setWindowTitle("Успех")
+            msg.setText("Выполнено")
+            msg.setInformativeText("Учетная запись создана успешно")
+        except Exception as err:  # mc.Error
+            msg = QMessageBox()
+            msg.setIcon(QMessageBox.Critical)
+            msg.setWindowTitle("Ошибка")
+            msg.setText("Внимание")
+            msg.setInformativeText("Проверьте введенные данные!")
+            msg.exec_()
 
     def setupUi(self, AdminFom):
         AdminFom.setObjectName("AdminFom")
@@ -23,9 +101,11 @@ class Ui_AdminFom(object):
         self.Factory.setObjectName("Factory")
         self.tab_3 = QtWidgets.QWidget()
         self.tab_3.setObjectName("tab_3")
-        self.tableView = QtWidgets.QTableView(self.tab_3)
-        self.tableView.setGeometry(QtCore.QRect(10, 60, 521, 441))
-        self.tableView.setObjectName("tableView")
+        self.tableWidgetFactory = QtWidgets.QTableWidget(self.tab_3)
+        self.tableWidgetFactory.setGeometry(QtCore.QRect(10, 60, 531, 441))
+        self.tableWidgetFactory.setObjectName("tableWidgetFactory")
+        self.tableWidgetFactory.setColumnCount(0)
+        self.tableWidgetFactory.setRowCount(0)
         self.groupBox = QtWidgets.QGroupBox(self.tab_3)
         self.groupBox.setGeometry(QtCore.QRect(550, 50, 321, 451))
         self.groupBox.setObjectName("groupBox")
@@ -33,7 +113,7 @@ class Ui_AdminFom(object):
         self.saveData.setGeometry(QtCore.QRect(240, 420, 71, 21))
         self.saveData.setObjectName("saveData")
         self.choosenTableForFactory = QtWidgets.QComboBox(self.tab_3)
-        self.choosenTableForFactory.setGeometry(QtCore.QRect(10, 30, 131, 18))
+        self.choosenTableForFactory.setGeometry(QtCore.QRect(10, 30, 131, 21))
         self.choosenTableForFactory.setObjectName("choosenTableForFactory")
         self.tabelLabel_3 = QtWidgets.QLabel(self.tab_3)
         self.tabelLabel_3.setGeometry(QtCore.QRect(10, 10, 131, 18))
@@ -42,6 +122,10 @@ class Ui_AdminFom(object):
         font.setPointSize(14)
         self.tabelLabel_3.setFont(font)
         self.tabelLabel_3.setObjectName("tabelLabel_3")
+        self.showTableForFactory = QtWidgets.QPushButton(self.tab_3)
+        self.showTableForFactory.setGeometry(QtCore.QRect(150, 30, 75, 23))
+        self.showTableForFactory.setObjectName("showTableForFactory")
+        self.showTableForFactory.clicked.connect(self.showTableForFactoryClick)
         self.Factory.addTab(self.tab_3, "")
         self.tab_4 = QtWidgets.QWidget()
         self.tab_4.setObjectName("tab_4")
@@ -51,6 +135,7 @@ class Ui_AdminFom(object):
         self.AddUserButton = QtWidgets.QPushButton(self.groupBox_2)
         self.AddUserButton.setGeometry(QtCore.QRect(140, 110, 75, 23))
         self.AddUserButton.setObjectName("AddUserButton")
+        self.AddUserButton.clicked.connect(self.insertDataIntoUsers)
         self.label = QtWidgets.QLabel(self.groupBox_2)
         self.label.setGeometry(QtCore.QRect(20, 33, 47, 13))
         font = QtGui.QFont()
@@ -93,12 +178,16 @@ class Ui_AdminFom(object):
         self.UserRoleCreate.addItem("")
         self.UserRoleCreate.addItem("")
         self.verticalLayout_2.addWidget(self.UserRoleCreate)
-        self.tableView_2 = QtWidgets.QTableView(self.tab_4)
-        self.tableView_2.setGeometry(QtCore.QRect(10, 60, 521, 441))
-        self.tableView_2.setObjectName("tableView_2")
+        self.tableWidgetUsers = QtWidgets.QTableWidget(self.tab_4)
+        self.tableWidgetUsers.setGeometry(QtCore.QRect(10, 60, 521, 441))
+        self.tableWidgetUsers.setObjectName("tableWidgetUsers")
+        self.tableWidgetUsers.setColumnCount(0)
+        self.tableWidgetUsers.setRowCount(0)
         self.choosenTableForUsers = QtWidgets.QComboBox(self.tab_4)
-        self.choosenTableForUsers.setGeometry(QtCore.QRect(10, 30, 131, 18))
+        self.choosenTableForUsers.setGeometry(QtCore.QRect(10, 30, 131, 21))
         self.choosenTableForUsers.setObjectName("choosenTableForUsers")
+        self.choosenTableForUsers.addItem("")
+        self.choosenTableForUsers.addItem("")
         self.tabelLabel_4 = QtWidgets.QLabel(self.tab_4)
         self.tabelLabel_4.setGeometry(QtCore.QRect(10, 10, 131, 18))
         font = QtGui.QFont()
@@ -106,10 +195,15 @@ class Ui_AdminFom(object):
         font.setPointSize(14)
         self.tabelLabel_4.setFont(font)
         self.tabelLabel_4.setObjectName("tabelLabel_4")
+        self.showTableForUsers = QtWidgets.QPushButton(self.tab_4)
+        self.showTableForUsers.setGeometry(QtCore.QRect(150, 30, 75, 23))
+        self.showTableForUsers.setObjectName("showTableForUsers")
+        self.showTableForUsers.clicked.connect(self.showTableForUsersClick)
         self.Factory.addTab(self.tab_4, "")
         self.openOperatorForm_2 = QtWidgets.QPushButton(self.centralwidget)
         self.openOperatorForm_2.setGeometry(QtCore.QRect(10, 540, 181, 31))
         self.openOperatorForm_2.setObjectName("openOperatorForm_2")
+        self.openOperatorForm_2.clicked.connect(self.openOperForm)
         self.openReserchForm_2 = QtWidgets.QPushButton(self.centralwidget)
         self.openReserchForm_2.setGeometry(QtCore.QRect(200, 540, 181, 31))
         self.openReserchForm_2.setObjectName("openReserchForm_2")
@@ -135,7 +229,7 @@ class Ui_AdminFom(object):
         self.menubar.addAction(self.menu_2.menuAction())
 
         self.retranslateUi(AdminFom)
-        self.Factory.setCurrentIndex(0)
+        self.Factory.setCurrentIndex(1)
         QtCore.QMetaObject.connectSlotsByName(AdminFom)
 
     def retranslateUi(self, AdminFom):
@@ -144,6 +238,7 @@ class Ui_AdminFom(object):
         self.groupBox.setTitle(_translate("AdminFom", "Промышленные данные"))
         self.saveData.setText(_translate("AdminFom", "Сохранить"))
         self.tabelLabel_3.setText(_translate("AdminFom", "Таблица:"))
+        self.showTableForFactory.setText(_translate("AdminFom", "Показать"))
         self.Factory.setTabText(self.Factory.indexOf(self.tab_3), _translate("AdminFom", "Промышленные данные"))
         self.groupBox_2.setTitle(_translate("AdminFom", "Добавление учетных записей"))
         self.AddUserButton.setText(_translate("AdminFom", "Добавить"))
@@ -154,7 +249,11 @@ class Ui_AdminFom(object):
         self.UserRoleCreate.setItemText(0, _translate("AdminFom", "Оператор"))
         self.UserRoleCreate.setItemText(1, _translate("AdminFom", "Администратор"))
         self.UserRoleCreate.setItemText(2, _translate("AdminFom", "Разработчик модели"))
+        self.choosenTableForUsers.setCurrentText(_translate("AdminFom", "Пользователи"))
+        self.choosenTableForUsers.setItemText(0, _translate("AdminFom", "Пользователи"))
+        self.choosenTableForUsers.setItemText(1, _translate("AdminFom", "Роли"))
         self.tabelLabel_4.setText(_translate("AdminFom", "Таблица:"))
+        self.showTableForUsers.setText(_translate("AdminFom", "Показать"))
         self.Factory.setTabText(self.Factory.indexOf(self.tab_4), _translate("AdminFom", "Пользователи"))
         self.openOperatorForm_2.setText(_translate("AdminFom", "Перейти в режим оператора"))
         self.openReserchForm_2.setText(_translate("AdminFom", "Перейти в режим исследователя"))
