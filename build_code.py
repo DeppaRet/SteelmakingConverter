c635a71afb4105a695a2f115dd70a3434d491e9f
Ilya Levinskii <56347883+DeppaRet@users.noreply.github.com>
2024-05-14 11:05:03 +0300

    Build code

---
+++ build_code.py
@@ -0,0 +1,19 @@
+import sys
+from cx_Freeze import setup, Executable
+
+# Замените "ваш_скрипт.py" на имя вашего скрипта Python
+executables = [Executable("main.py")]
+
+# Здесь можно указать другие параметры сборки
+build_exe_options = {
+    "include_files": ["dev.ini", "pictures"],
+    "excludes": [],
+}
+
+setup(
+    name="Converter",
+    version="1.0",
+    description="Описание вашего приложения",
+    options={"build_exe": build_exe_options},
+    executables=executables
+)
